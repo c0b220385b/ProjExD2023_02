918764b6986b436444fc095ed34d4f8501191278
c0b220385b <c0b220385b@edu.teu.ac.jp>
2023-06-20 15:16:28 +0900

    練習3

--- ex02/dodge_bomb.py
+++ ex02/dodge_bomb.py
@@ -4,7 +4,12 @@ import random
 
 
 WIDTH, HEIGHT = 1600, 900
-
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +17,9 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    # こうかとんSurface（kk_img）からこうかとんRect（kk_rct）を抽出する
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
     bd_img = pg.Surface((20, 20)) #練習1
     bd_img.set_colorkey((0, 0, 0)) #黒い部分を透明にする
     pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
@@ -29,8 +37,16 @@ def main():
             if event.type == pg.QUIT: 
                 return
 
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]  # 合計移動量
+        for k, mv in delta.items():
+            if key_lst[k]: 
+                sum_mv[0] += mv[0]
+                sum_mv[1] += mv[1]
+        kk_rct.move_ip(sum_mv)
+
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bd_rct.move_ip(vx, vy) #練習2
         screen.blit(bd_img, bd_rct)
         pg.display.update()
